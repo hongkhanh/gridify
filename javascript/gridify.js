1e0057f25a38fc01074406bdcba755c5bdeb6455
Nguyen Hong Khanh <khanh0203@gmail.com>
2014-10-05 19:34:55 +0700

    Update gridify.js

--- javascript/gridify.js
+++ javascript/gridify.js
@@ -56,7 +56,7 @@ Element.prototype.gridify = function (options)
                 item_margin = parseInt(options.margin || 0),
                 item_width = parseInt(options.max_width || options.width || 220),
                 column_count = Math.max(Math.floor(width/(item_width + item_margin)),1),
-                left = column_count == 1 ? -item_margin/2 : (width % (item_width + item_margin)) / 2,
+                left = column_count == 1 ? item_margin/2 : (width % (item_width + item_margin)) / 2,
                 columns = [];
             if (options.max_width)
             {
