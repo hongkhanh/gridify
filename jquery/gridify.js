d3232f91bee770f5743199f0fa1ba04c5c42fe45
Nguyen Hong Khanh <khanh0203@gmail.com>
2014-10-05 17:30:06 +0700

    Update gridify.js

--- jquery/gridify.js
+++ jquery/gridify.js
@@ -33,7 +33,7 @@ $.fn.extend({
                     item_margin = parseInt(options.margin || 0),
                     item_width = parseInt(options.max_width || options.width || 220),
                     column_count = Math.max(Math.floor(width/(item_width + item_margin)),1),
-                    left = column_count == 1 ? -item_margin/2 : (width % (item_width + item_margin)) / 2,
+                    left = column_count == 1 ? item_margin/2 : (width % (item_width + item_margin)) / 2,
                     columns = [];
 
                 if (options.max_width) {
